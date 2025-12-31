0c200e85af901f98ae3f6f5f28722a3e91f44211
mohamad70623 <mohammad70623@gmail.com>
2025-12-31 20:24:45 +0600

    data validated by in built function

--- _pydantic_why.py
+++ _pydantic_why.py
@@ -1,9 +1,11 @@
-from pydantic import BaseModel 
+from pydantic import BaseModel, EmailStr, AnyUrl
 from typing import List, Dict, Optional
 class Patient(BaseModel):
 
     name: str 
     age: int 
+    email: EmailStr
+    linkedin_url: AnyUrl
     weight: float 
     married: bool
     allergis: Optional[List[str]] = None
@@ -17,22 +19,28 @@ def insert_patient_data(patient: Patient):
     print(patient.age)
     print(patient.allergis)
     print(patient.married)
+    print(patient.email)
+    print(patient.linkedin_url)
     print('inserted')
 
 def update_patient_data(patient: Patient):
 
     print(patient.name)
     print(patient.age)
+    print(patient.email)
     print(patient.allergis)
     print(patient.married)
+    print(patient.linkedin_url)
     print('updated')
 
 patient_info = {'name': 'Mohammad', 
                 'age': 30, 
                 'weight': 57.5,
                 'married': False,
+                'email': 'mohammad70623@gmail.com',
+                'linkedin_url': 'https://www.linkedin.com/in/mohammad-51a10a287/',
                 'allergis':['dust', 'pollen'], 
-                'contact_details': {'Email': 'mohammad70623@gmail.com',
+                'contact_details': {
                                     'Phone': '0183597062',
                                     'current address': 'Ashulia'}}
 
