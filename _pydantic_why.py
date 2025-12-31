81ad0801589c6f18189335c4dbbd1380e5d53cea
mohamad70623 <mohammad70623@gmail.com>
2025-12-31 19:38:16 +0600

    update_patient_info function added

--- _pydantic_why.py
+++ _pydantic_why.py
@@ -12,6 +12,12 @@ def insert_patient_data(patient: Patient):
     print(patient.age)
     print('inserted')
 
+def update_patient_data(patient: Patient):
+
+    print(patient.name)
+    print(patient.age)
+    print('updated')
+
 patient_info = {'name': 'Mohammad', 'age': 30}
 
 patient1 = Patient(**patient_info)
