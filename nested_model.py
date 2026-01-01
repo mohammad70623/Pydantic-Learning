b8ec7f6f04f99411f6a27dc7435349e53fa6838c
mohamad70623 <mohammad70623@gmail.com>
2026-01-01 15:15:51 +0600

    nested model complete

---
+++ nested_model.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel 
+
+class Address(BaseModel):
+
+    city: str 
+    state: str 
+    pin: str 
+
+class Patient(BaseModel):
+
+    name: str 
+    gender: str 
+    age: int 
+    address: Address
+    
+address_dict= {'city': 'Dhaka',
+               'state': 'Dhaka',
+               'pin': '4710'}
+address1 = Address(**address_dict)
+
+patient_dict = {'name':'Mohammad',
+                'gender': 'Male',
+                'age': 22,
+                'address': address1}
+patient1 = Patient(**patient_dict)
+
+print(patient1)
+print(patient1.name)
+print(patient1.address.city)
+print(patient1.address.pin)
